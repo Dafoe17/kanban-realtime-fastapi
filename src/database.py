ce498796052a4ff9ab58ba083801e02a6620f8bb
Dafoe17 <ram.and.rem207@gmail.com>
2025-12-15 04:28:54 +0300

    feat(alembic): add alembic revision

--- src/database.py
+++ src/database.py
@@ -8,3 +8,17 @@ engine = create_engine(settings.DATABASE_URL)
 Session_local = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 Base = declarative_base()
+
+from src.models import (  # noqa: E402,F401
+    Board,
+    Card,
+    CardTagAssociation,
+    Checklist,
+    ChecklistItem,
+    Column,
+    Comment,
+    Invite,
+    Tag,
+    User,
+    UserBoardPreference,
+)
